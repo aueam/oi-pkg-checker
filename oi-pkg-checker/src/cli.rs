3428f59bc9ee3db31b7e9b5319d8f47c56c232ca
Maroš Telka <maros@telka.sk>
2023-10-08 15:02:55 +0200

    Remove `update-assets` command
    Fixes #10

--- oi-pkg-checker/src/cli.rs
+++ oi-pkg-checker/src/cli.rs
@@ -17,53 +17,21 @@ pub enum Commands {
         debug: bool,
     },
 
-    /// manipulate with data
-    Data {
-        #[command(subcommand)]
-        data_commands: Option<DataCommands>,
-    },
-
-    /// Prints information about fmri and what packages need fmri.
-    CheckFMRI {
-        /// checking valid fmri
-        fmri: String,
-
-        /// path to oi userland repo (absolute path)
-        #[arg(long, value_name = "PATH")]
-        repo_path: PathBuf,
-
-        /// set debug on
-        #[arg(short, long, default_value = "false")]
-        debug: bool,
-    },
-}
-
-#[derive(Subcommand)]
-pub enum DataCommands {
-    /// update catalogs
-    UpdateAssets {
-        /// path to catalog.dependency.C
-        #[arg(short, long, value_name = "FILE")]
-        catalog: PathBuf,
-
-        /// path to catalog.encumbered.dependency.C
-        #[arg(short, long, value_name = "FILE")]
-        encumbered_catalog: PathBuf,
-
-        /// path to oi userland repo
-        #[arg(long, value_name = "PATH")]
-        repo_path: PathBuf,
-    },
-
     /// Run analyze
     Run {
         /// load catalogs (absolute paths)
         #[arg(long, value_name = "FILE")]
         catalog: Vec<PathBuf>,
 
-        /// path to oi userland repo (absolute path)
-        #[arg(long, value_name = "PATH")]
-        repo_path: PathBuf,
+        /// set debug on
+        #[arg(short, long, default_value = "false")]
+        debug: bool,
+    },
+
+    /// Prints information about fmri and what packages need fmri.
+    CheckFMRI {
+        /// checking valid fmri
+        fmri: String,
 
         /// set debug on
         #[arg(short, long, default_value = "false")]
--- oi-pkg-checker/src/main.rs
+++ oi-pkg-checker/src/main.rs
@@ -1,17 +1,14 @@
 mod logger;
 mod cli;
 
-use std::ffi::OsStr;
-use std::fs::{File, remove_file};
 use std::io::{Write};
 use std::path::{Path, PathBuf};
-use std::process::{Command, exit};
+use std::process::exit;
 use clap::Parser;
 use log::{LevelFilter, debug, error, info};
-use reqwest::blocking::get;
 use reqwest::IntoUrl;
 use fmri::FMRI;
-use crate::cli::{Args, Commands, DataCommands};
+use crate::cli::{Args, Commands};
 use crate::logger::Logger;
 use oi_pkg_checker_core::{
     MissingComponentForPackageList, NonExistingRequiredPackageList, ObsoletedRequiredPackageList,
@@ -64,9 +61,8 @@ fn main() {
                     exit(0);
                 }
                 Commands::CheckFMRI {
-                    repo_path
-                    , fmri
-                    , debug
+                    fmri,
+                    debug
                 } => {
                     debug_on(debug);
 
@@ -108,7 +104,7 @@ fn main() {
                         }
                     }
 
-                    let components_path = PathBuf::from(format!("{}/components", repo_path.to_string_lossy()));
+                    let components_path = PathBuf::from("assets/oi-userland/components");
 
                     let component_packages_list = ComponentPackagesList::new(
                         &components_path.clone(),
@@ -128,134 +124,103 @@ fn main() {
 
                     exit(0);
                 }
-                Commands::Data { data_commands } => {
-                    match data_commands {
-                        None => {}
-                        Some(command) => {
-                            match command {
-                                DataCommands::UpdateAssets {
-                                    catalog,
-                                    encumbered_catalog,
-                                    repo_path
-                                } => {
-                                    download_file("https://pkg.openindiana.org/hipster/catalog/1/catalog.dependency.C", catalog);
-                                    download_file("https://pkg.openindiana.org/hipster-encumbered/catalog/1/catalog.dependency.C", encumbered_catalog);
+                Commands::Run {
+                    catalog
+                    , debug
+                } => {
+                    debug_on(debug);
 
-                                    let command = Command::new("sh")
-                                        .arg("-c")
-                                        .arg(format!("cd {:?} && git pull", repo_path))
-                                        .output()
-                                        .expect("failed to run command");
-
-                                    let exit_code = command.status.code().unwrap();
-                                    if exit_code != 0 {
-                                        error!("{:?}", String::from_utf8_lossy(&command.stderr));
-                                        panic!("Cannot update OpenIndiana/oi-userland repo, exit code: {}", exit_code)
-                                    }
-                                    exit(0);
-                                }
-                                DataCommands::Run {
-                                    catalog
-                                    , repo_path
-                                    , debug
-                                } => {
-                                    debug_on(debug);
-
-                                    let mut missing_component_for_package_list = MissingComponentForPackageList::new();
-                                    let mut renamed_package_in_component_list = RenamedPackageInComponentList::new();
-                                    let mut un_runnable_make_command_list = UnRunnableMakeCommandList::new();
-                                    let mut non_existing_required_package_list = NonExistingRequiredPackageList::new();
-                                    let mut obsolete_required_package_list = ObsoletedRequiredPackageList::new();
-                                    let mut partly_obsolete_required_package_list = PartlyObsoletedRequiredPackageList::new();
-                                    let mut useless_components_list = UselessComponentsList::new();
+                    let mut missing_component_for_package_list = MissingComponentForPackageList::new();
+                    let mut renamed_package_in_component_list = RenamedPackageInComponentList::new();
+                    let mut un_runnable_make_command_list = UnRunnableMakeCommandList::new();
+                    let mut non_existing_required_package_list = NonExistingRequiredPackageList::new();
+                    let mut obsolete_required_package_list = ObsoletedRequiredPackageList::new();
+                    let mut partly_obsolete_required_package_list = PartlyObsoletedRequiredPackageList::new();
+                    let mut useless_components_list = UselessComponentsList::new();
 
 
-                                    let mut components = Components::new();
+                    let mut components = Components::new();
 
-                                    let components_path = PathBuf::from(format!("{}/components", repo_path.to_string_lossy()));
+                    let components_path = PathBuf::from("assets/oi-userland/components");
 
-                                    let catalog_path = &components_path.clone();
+                    let catalog_path = &components_path.clone();
 
-                                    let encumbered_catalog_path = &PathBuf::from((components_path.to_string_lossy() + "/encumbered").to_string());
+                    let encumbered_catalog_path = &PathBuf::from((components_path.to_string_lossy() + "/encumbered").to_string());
 
-                                    match components.load(Assets::Catalogs(
-                                        catalog.clone()), catalog_path, encumbered_catalog_path) {
-                                        Ok(_) => {}
-                                        Err(problem) => match problem {
-                                            Ok(problems) => renamed_package_in_component_list += problems,
-                                            Err(_) => {}
-                                        }
-                                    }
+                    match components.load(Assets::Catalogs(
+                        catalog.clone()), catalog_path, encumbered_catalog_path) {
+                        Ok(_) => {}
+                        Err(problem) => match problem {
+                            Ok(problems) => renamed_package_in_component_list += problems,
+                            Err(_) => {}
+                        }
+                    }
 
-                                    match components.load(Assets::OpenIndianaOiUserlandGit {
-                                        load_component_list: true,
-                                        load_build_dependencies: true,
-                                        load_test_dependencies: true,
-                                        load_system_build_dependencies: true,
-                                        load_system_test_dependencies: true,
-                                    }, catalog_path, encumbered_catalog_path) {
-                                        Ok(_) => {}
-                                        Err(error) => match error {
-                                            Ok(_) => {}
-                                            Err((missing, renamed, un_runnable)) => {
-                                                missing_component_for_package_list += missing;
-                                                renamed_package_in_component_list += renamed;
-                                                un_runnable_make_command_list += un_runnable;
-                                            }
-                                        }
-                                    }
-
-                                    match components.check_dependency_validity() {
-                                        Ok(_) => {}
-                                        Err((non_existing, obsolete, partly_obsolete)) => {
-                                            non_existing_required_package_list += non_existing;
-                                            obsolete_required_package_list += obsolete;
-                                            partly_obsolete_required_package_list += partly_obsolete;
-                                        }
-                                    }
-
-                                    match components.get_useless_components() {
-                                        Ok(_) => {}
-                                        Err(useless) => {
-                                            useless_components_list += useless
-                                        }
-                                    }
-
-                                    let renamed_needs_renamed_list = components.check_if_renamed_needs_renamed();
-
-                                    components.clone().serialize(data_path);
-
-                                    report(
-                                        missing_component_for_package_list.clone(),
-                                        renamed_needs_renamed_list.clone(),
-                                        renamed_package_in_component_list.clone(),
-                                        un_runnable_make_command_list.clone(),
-                                        non_existing_required_package_list.clone(),
-                                        obsolete_required_package_list.clone(),
-                                        partly_obsolete_required_package_list.clone(),
-                                        useless_components_list.clone(),
-                                        &components,
-                                    );
-
-
-                                    let problems = Problems::new(
-                                        missing_component_for_package_list,
-                                        renamed_needs_renamed_list,
-                                        renamed_package_in_component_list,
-                                        un_runnable_make_command_list,
-                                        non_existing_required_package_list,
-                                        obsolete_required_package_list,
-                                        partly_obsolete_required_package_list,
-                                        useless_components_list,
-                                    );
-
-                                    problems.serialize(problems_path);
-
-                                    exit(0);
-                                }
+                    match components.load(Assets::OpenIndianaOiUserlandGit {
+                        load_component_list: true,
+                        load_build_dependencies: true,
+                        load_test_dependencies: true,
+                        load_system_build_dependencies: true,
+                        load_system_test_dependencies: true,
+                    }, catalog_path, encumbered_catalog_path) {
+                        Ok(_) => {}
+                        Err(error) => match error {
+                            Ok(_) => {}
+                            Err((missing, renamed, un_runnable)) => {
+                                missing_component_for_package_list += missing;
+                                renamed_package_in_component_list += renamed;
+                                un_runnable_make_command_list += un_runnable;
                             }
                         }
                     }
+
+                    match components.check_dependency_validity() {
+                        Ok(_) => {}
+                        Err((non_existing, obsolete, partly_obsolete)) => {
+                            non_existing_required_package_list += non_existing;
+                            obsolete_required_package_list += obsolete;
+                            partly_obsolete_required_package_list += partly_obsolete;
+                        }
+                    }
+
+                    match components.get_useless_components() {
+                        Ok(_) => {}
+                        Err(useless) => {
+                            useless_components_list += useless
+                        }
+                    }
+
+                    let renamed_needs_renamed_list = components.check_if_renamed_needs_renamed();
+
+                    components.clone().serialize(data_path);
+
+                    report(
+                        missing_component_for_package_list.clone(),
+                        renamed_needs_renamed_list.clone(),
+                        renamed_package_in_component_list.clone(),
+                        un_runnable_make_command_list.clone(),
+                        non_existing_required_package_list.clone(),
+                        obsolete_required_package_list.clone(),
+                        partly_obsolete_required_package_list.clone(),
+                        useless_components_list.clone(),
+                        &components,
+                    );
+
+
+                    let problems = Problems::new(
+                        missing_component_for_package_list,
+                        renamed_needs_renamed_list,
+                        renamed_package_in_component_list,
+                        un_runnable_make_command_list,
+                        non_existing_required_package_list,
+                        obsolete_required_package_list,
+                        partly_obsolete_required_package_list,
+                        useless_components_list,
+                    );
+
+                    problems.serialize(problems_path);
+
+                    exit(0);
                 }
             }
         }
@@ -274,21 +239,4 @@ fn debug_on(debug: &bool) {
         log::set_max_level(LevelFilter::Debug);
         debug!("debug is on");
     }
-}
-
-fn download_file<T: IntoUrl, P: AsRef<Path> + AsRef<OsStr> + ?Sized>(url: T, path: &P) {
-    if Path::new(&path).exists() {
-        remove_file(&path).expect("Failed to remove existing file");
-    }
-
-    File::create(&path)
-        .expect("Failed to create file")
-        .write_all(
-            get(url)
-                .expect("Failed to download file")
-                .text()
-                .unwrap()
-                .as_bytes()
-        )
-        .expect("TODO: panic message");
 }
